a30eb84fa0228c7b7757ec2aaf980181af243abb
Matias Celentano <matiascelentano562@gmail.com>
2024-05-04 16:07:16 -0300

    test:listener en player para obtener informacion de la cancion a traves de la metadata

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.media.MediaPlayer;
+import android.media.browse.MediaBrowser;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import androidx.activity.EdgeToEdge;
@@ -9,7 +12,12 @@ import androidx.core.content.res.ResourcesCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.media3.common.MediaItem;
+import androidx.media3.common.MediaMetadata;
+import androidx.media3.common.Player;
+import androidx.media3.exoplayer.*;
 import androidx.media3.exoplayer.ExoPlayer;
+import androidx.media3.exoplayer.MetadataRetriever;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -28,23 +36,36 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //Preparando Exoplayer
+        ExoPlayer player = new ExoPlayer.Builder(this).build();
+        MediaItem song1 = MediaItem.fromUri(Uri.parse("asset:///daft_punk_crescendolls.mp3"));
+        player.setMediaItem(song1);
+        player.prepare();
+        //Probando conseguir informacion de la cancion a partir de la metadata
+        //MediaMetadata mediaMetadata = player.getMediaMetadata();
+       // Log.i("song", "cancion: " + mediaMetadata.title);
+        player.addListener(new Player.Listener() {
+                   @Override
+                   public void onMediaMetadataChanged(MediaMetadata mediaMetadata) {
+                       //Now get posted values like this
+                       Log.i("song", "cancion: " + mediaMetadata.title);
+                   }
+               }
+        );
+
+        //Listener en boton play/pause para reproducir/pausar cancion y cambiar de icono
         MaterialButton buttonPlayPause = findViewById(R.id.buttonPlayPause);
-        MediaPlayer song = MediaPlayer.create(this, R.raw.daft_punk_crescendolls);
-        Log.i("song", "cancion: " + Arrays.toString(song.getTrackInfo()));
         buttonPlayPause.addOnCheckedChangeListener((materialButton, b) -> {
             Log.i("blah", "funciona: " + b);
-            if(b){
+            if (b) {
                 materialButton.setIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.pause_button, null));
-                song.start();
-            }else if(!b && song.isPlaying()) {
+                player.play();
+            } else if (!b && player.isPlaying()) {
                 materialButton.setIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.play_button, null));
-                song.pause();
-            }else{
+                player.pause();
+            } else {
                 materialButton.setIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.play_button, null));
             }
-        });
-        song.setOnCompletionListener(mediaPlayer -> {
-            buttonPlayPause.setChecked(false);
         });
     }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/Song.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Song.java
@@ -1,10 +1,14 @@
 package com.example.myapplication;
 
+import androidx.media3.exoplayer.ExoPlayer;
+
 public class Song {
     private Integer id;
     private String name;
     private String albumName;
     private Integer duration;
+    private String fileName;
+    private String directoryPath;
     //Getters & Setters
     public Integer getId() {
         return id;
@@ -29,6 +33,27 @@ public class Song {
     }
     public void setDuration(Integer duration) {
         this.duration = duration;
+    }
+    public String getFileName() {
+        return fileName;
+    }
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+    public String getDirectoryPath() {
+        return directoryPath;
+    }
+    public void setDirectoryPath(String directoryPath) {
+        this.directoryPath = "asset:///"+ this.getFileName();
+    }
+
+    public Song(Integer id, String name, String albumName, Integer duration, String fileName) {
+        setId(id);
+        setName(name);
+        setAlbumName(albumName);
+        setDuration(duration);
+        setFileName(fileName);
+        setDirectoryPath(fileName);
     }
 
 }
