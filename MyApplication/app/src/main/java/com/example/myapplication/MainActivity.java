afb732a71e12c5d7d0a0d6dbdad246b09dfcd232
Matias Celentano <matiascelentano562@gmail.com>
2024-05-03 21:00:23 -0300

    fix:borrando imports que no fueron utilizados, cambiando listener dde button_play_pause a lambda

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.myapplication;
 
-import android.content.res.ColorStateList;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
@@ -13,7 +9,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import com.google.android.material.button.MaterialButton;
-import com.google.android.material.button.MaterialButtonToggleGroup;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -29,16 +24,12 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         MaterialButton buttonPlayPause = findViewById(R.id.button_play_pause);
-        buttonPlayPause.addOnCheckedChangeListener(new MaterialButton.OnCheckedChangeListener() {
-
-            @Override
-            public void onCheckedChanged(MaterialButton materialButton, boolean b) {
-                Log.i("Click", "estado: "+b);
-                if(b == true){
-                    materialButton.setIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.play_button, null));
-                }else {
-                    materialButton.setIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.pause_button, null));
-                }
+        buttonPlayPause.addOnCheckedChangeListener((materialButton, b) -> {
+            Log.i("Click", "estado: "+b);
+            if(b){
+                materialButton.setIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.play_button, null));
+            }else {
+                materialButton.setIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.pause_button, null));
             }
         });
     }
