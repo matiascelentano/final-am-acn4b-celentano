174b9c7c3087edf0c90956ee8dc37cc771107f98
Matias Celentano <matiascelentano562@gmail.com>
2024-06-21 17:41:47 -0300

    feat: cambiando pantalla principal de MainActivity a home

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -32,55 +32,5 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        initRecyclerView();
-        //Boton Play/Pause
-        MaterialButton buttonPlayPause = findViewById(R.id.buttonPlayPause);
-        //Preparando Exoplayer
-        ExoPlayer player = createExoPlayerPlaylist();
-        player.addListener(new Player.Listener(){
-                   @Override
-                   //Probando conseguir informacion de la cancion a partir de la metadata
-                   public void onMediaMetadataChanged(MediaMetadata mediaMetadata) {
-                       Log.i("song", "cancion: " + mediaMetadata.title);
-                   }
-                    @Override
-                    //TODO: Implementar funcion para que se resetee la playlist al terminar de reproducir todos los items en el player
-                    public void onIsPlayingChanged(boolean isPlaying) {
-                        if (isPlaying) {
-
-                        } else {
-                            buttonPlayPause.setIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.play_button, null));
-                        }
-                    }
-        });
-        //Listener en boton play/pause para reproducir/pausar cancion y cambiar de icono
-        buttonPlayPause.addOnCheckedChangeListener((materialButton, b) -> {
-            if (b) {
-                materialButton.setIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.pause_button, null));
-                player.play();
-            } else if (!b && player.isPlaying()) {
-                materialButton.setIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.play_button, null));
-                player.pause();
-            } else {
-                materialButton.setIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.play_button, null));
-            }
-        });
-
     }
-    public ExoPlayer createExoPlayerPlaylist(){
-        ExoPlayer player = new ExoPlayer.Builder(this).build();
-        MediaItem song1 = MediaItem.fromUri(Uri.parse("asset:///emotion_engine.mp3"));
-        MediaItem song2 = MediaItem.fromUri(Uri.parse("asset:///crescendolls.mp3"));
-        player.addMediaItem(song1);
-        player.addMediaItem(song2);
-        player.prepare();
-        return player;
-    }
-    private void initRecyclerView(){
-        RecyclerView recyclerView = findViewById(R.id.recyclerViewId);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        SongAdapter songAdapter = new SongAdapter(SongList.getSongs());
-        recyclerView.setAdapter(songAdapter);
-    }
-
 }
