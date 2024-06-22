4a40f0170e8c4fe04f05ae0e96538710b67ccd5c
Matias Celentano <matiascelentano562@gmail.com>
2024-06-22 14:58:34 -0300

    feat:creando clase artista y funcion de prueba lista de artistas

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Artist.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+public class Artist {
+    private int id;
+    private String name;
+    private int img;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public Artist(int id, String name, int img) {
+        this.id = id;
+        this.name = name;
+        this.img = img;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", img=" + img +
+                '}';
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/ArtistList.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import java.util.ArrayList;
+
+public class ArtistList {
+    public static ArrayList<Artist> getArtists(){
+        int ram = R.drawable.album_cover;
+        int vkiss = R.drawable.album_cover2;
+        int discovery = R.drawable.album_cover3;
+        int colorandshape = R.drawable.album_cover4;
+        int audioslave = R.drawable.album_cover5;
+
+        Artist artist1 = new Artist(1,"Artista Prueba 1", ram);
+        Artist artist2 = new Artist(2,"Artista Prueba 2", vkiss);
+        Artist artist3 = new Artist(3,"Artista Prueba 3", discovery);
+        Artist artist4 = new Artist(4,"Artista Prueba 4", audioslave);
+        Artist artist5 = new Artist(5,"Artista Prueba 5", colorandshape);
+        Artist artist6 = new Artist(6,"Artista Prueba 6", ram);
+
+        ArrayList<Artist> artists = new ArrayList<>();
+        artists.add(artist1);
+        artists.add(artist2);
+        artists.add(artist3);
+        artists.add(artist4);
+        artists.add(artist5);
+        artists.add(artist6);
+
+        return artists;
+    }
+}
