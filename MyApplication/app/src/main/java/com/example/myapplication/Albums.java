54c0761ae4f43596e1aba4926460e23d26d1311f
Matias Celentano <matiascelentano562@gmail.com>
2024-06-28 17:49:22 -0300

    style: ajustando estilos mainactivity

--- MyApplication/app/src/main/java/com/example/myapplication/Albums.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Albums.java
@@ -14,11 +14,15 @@ public class Albums {
         Album album2 = new Album(2,"Random Access Memory",ram);
         Album album3 = new Album(3,"Discovery",discovery);
         Album album4 = new Album(4,"Audioslave",audioslave);
+        Album album5 = new Album(5,"Audioslave",audioslave);
+        Album album6 = new Album(6,"Audioslave",audioslave);
 
         albums.add(album1);
         albums.add(album2);
         albums.add(album3);
         albums.add(album4);
+        albums.add(album5);
+        albums.add(album6);
 
         return albums;
     }
