c443773769408e372a3fd735db697942a4a9ecfe
Matias Celentano <matiascelentano562@gmail.com>
2024-06-28 16:17:51 -0300

    feat: creando adapter y viewholder de albumrecyclerview en main

---
+++ MyApplication/app/src/main/java/com/example/myapplication/adapter/AlbumAdapter.java
@@ -0,0 +1,45 @@
+package com.example.myapplication.adapter;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.Album;
+import com.example.myapplication.R;
+
+import java.util.ArrayList;
+
+public class AlbumAdapter extends RecyclerView.Adapter<AlbumViewHolder>{
+    private ArrayList<Album> albums;
+    public AlbumAdapter(ArrayList<Album> albums) {
+        this.albums = albums;
+    }
+
+    @NonNull
+    @Override
+    public AlbumViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.item_main_album_recycler, parent, false);
+        return new AlbumViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AlbumViewHolder holder, int position) {
+        Album item = albums.get(position);
+        holder.itemView.setTag(position);
+        holder.itemView.setOnClickListener(view -> {
+            int position1 = (int) view.getTag();
+            Log.i("album", "Position: " + position);
+        });
+        holder.render(item);
+    }
+    @Override
+    public int getItemCount() {
+        return albums == null ? 0 : albums.size();
+    }
+
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/adapter/AlbumViewHolder.java
@@ -0,0 +1,24 @@
+package com.example.myapplication.adapter;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.Album;
+import com.example.myapplication.R;
+
+public class AlbumViewHolder extends RecyclerView.ViewHolder {
+    private final ImageView albumImage;
+    private final TextView albumName;
+    public AlbumViewHolder(@NonNull View itemView) {
+        super(itemView);
+        albumImage = itemView.findViewById(R.id.albumImageRecycler);
+        albumName = itemView.findViewById(R.id.albumTitle);
+    }
+    public void render(Album artist){
+        albumImage.setImageResource(artist.getImg());
+        albumName.setText(artist.getName());
+    }
+}
