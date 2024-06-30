ce20c0e535bcaeac407d036899ed68d5b4564397
Matias Celentano <matiascelentano562@gmail.com>
2024-06-30 17:14:47 -0300

    style:ajuste de estilos register y login layout

--- soundwaves/app/src/main/java/com/example/soundwaves/LoginActivity.java
+++ soundwaves/app/src/main/java/com/example/soundwaves/LoginActivity.java
@@ -4,23 +4,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
-    EditText loginEmail, loginPassword;
+    TextInputEditText loginEmail, loginPassword;
     Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,20 +29,17 @@ public class LoginActivity extends AppCompatActivity {
             return insets;
         });
 
-        loginEmail = findViewById(R.id.loginEmail);
-        loginPassword = findViewById(R.id.loginPassword);
+        loginEmail = findViewById(R.id.loginEmailInput);
+        loginPassword = findViewById(R.id.loginPasswordInput);
         loginButton = findViewById(R.id.loginButton);
 
-        loginButton.setOnClickListener(view -> login(view));
+        loginButton.setOnClickListener(this::login);
     }
     public void login(View view){
         mAuth.signInWithEmailAndPassword(loginEmail.getText().toString(),loginPassword.getText().toString())
-            .addOnCompleteListener(this,new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()){
-                        Log.i("authf",mAuth.getCurrentUser().getEmail());
-                    }
+            .addOnCompleteListener(this, task -> {
+                if (task.isSuccessful()){
+                    Log.i("authf",mAuth.getCurrentUser().getEmail());
                 }
             });
     }
